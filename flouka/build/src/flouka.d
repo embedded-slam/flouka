51d6a74b76fe148dbaec8d7c2fae74ef1cc9307a
mohamed.g.ebrahim <mohamed.g.ebrahim@5f75e418-ef36-11de-bf31-4dec8810c1c1>
2009-12-22 21:27:54 +0000

    As per Karim request :)

---
+++ flouka/build/src/flouka.d
@@ -0,0 +1,4 @@
+src/flouka.d src/flouka.o: ../src/flouka.c \
+  /home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h
+
+/home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h:
