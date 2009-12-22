4e77c9401fc8c3b32ef7e10e01b22acb8f869a49
karim.e.morsy <karim.e.morsy@5f75e418-ef36-11de-bf31-4dec8810c1c1>
2009-12-22 21:13:04 +0000

    First Version

---
+++ trunk/flouka/build/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/flouka.c 
+
+OBJS += \
+./src/flouka.o 
+
+C_DEPS += \
+./src/flouka.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -DDEBUG -I"/home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
