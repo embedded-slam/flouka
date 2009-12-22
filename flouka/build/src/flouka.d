1cc6961fb3e1feea71a5b7c7fde930d2c6cb0f6f
mohamed.g.ebrahim <mohamed.g.ebrahim@5f75e418-ef36-11de-bf31-4dec8810c1c1>
2009-12-22 21:34:10 +0000

    Deleting the dependancies.

--- flouka/build/src/flouka.d
+++
@@ -1,4 +0,0 @@
-src/flouka.d src/flouka.o: ../src/flouka.c \
-  /home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h
-
-/home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h:
--- test_flouka/build/main.d
+++
@@ -1,7 +0,0 @@
-main.d main.o: ../main.c \
-  /home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h \
-  /home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka_wrapper.h
-
-/home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka.h:
-
-/home/SYSDSOFT/mgalal/Desktop/flouka_linux/flouka/include/flouka_wrapper.h:
